c7bd0982ffed373e331e17289cbc25fbb063a2ce
yuya304 <yuyaaoki34@gmail.com>
2023-05-15 23:35:03 +0900

    適切なコミットコメント

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
