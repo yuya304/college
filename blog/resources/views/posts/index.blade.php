9bd9ddfd738385d7ef1051f6a8eda221bbdd6b6b
yuya304 <yuyaaoki34@gmail.com>
2023-05-22 23:03:44 +0900

    college8-4

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -17,6 +17,7 @@
                 </div>
             @endforeach
         </div>
+        <a href='/posts/create'>create</a>
         <div class='paginate'>
             {{$posts->links()}}
         </div>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,4 +15,7 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
+
